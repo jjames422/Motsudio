09127fa9c3d94b7e68008cb6d8e411e2a4a74060
Jonathan <jjames422@me.com>
2023-08-01 15:34:17 -0400

    init

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -177,11 +177,11 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Award winning developer studio based in Denmark.
+            Award winning developer studio based in Charlotte.
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
             We are developer studio working at the intersection of design and
-            technology. It’s a really busy intersection though — a lot of our
+            technology. It\’s a really busy intersection though — a lot of our
             staff have been involved in hit and runs.
           </p>
         </FadeIn>
@@ -196,7 +196,7 @@ export default async function Home() {
         client={{ name: 'Phobia', logo: logoPhobiaDark }}
       >
         The team at Studio went above and beyond with our onboarding, even
-        finding a way to access the user’s microphone without triggering one of
+        finding a way to access the user\’s microphone without triggering one of
         those annoying permission dialogs.
       </Testimonial>
 
