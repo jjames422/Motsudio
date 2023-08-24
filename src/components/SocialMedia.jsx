111b67646b9f201975b36d305fe7c1abba4295dd
Jonathan <jjames422@me.com>
2023-08-24 17:01:17 -0400

    unhid facebook and twitter

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -58,9 +58,10 @@ function DribbbleIcon(props) {
 }
 
 export const socialMediaProfiles = [
-/*   { title: 'Facebook', href: 'https://facebook.com', icon: FacebookIcon },
-  { title: 'Instagram', href: 'https://instagram.com', icon: InstagramIcon },
-  { title: 'Twitter', href: 'https://twitter.com', icon: TwitterIcon },
+ { title: 'Facebook', href: 'https://www.facebook.com/motsingerstudios', icon: FacebookIcon },
+ { title: 'Twitter', href: 'https://twitter.com/motsingerstudio', icon: TwitterIcon },
+ /*   { title: 'Instagram', href: 'https://instagram.com', icon: InstagramIcon },
+
   { title: 'GitHub', href: 'https://github.com', icon: GitHubIcon },
   { title: 'Dribbble', href: 'https://dribbble.com', icon: DribbbleIcon }, */
 ]
