e3aff32f33545c9d12148835ecea7a28e2ef4708
Jonathan <jjames422@me.com>
2023-09-04 18:43:16 -0400

    swr

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -31,11 +31,7 @@ export default function Example() {
   return (
     <div className="flex items-start space-x-4">
       <div className="flex-shrink-0">
-        <img
-          className="inline-block h-10 w-10 rounded-full"
-          src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          alt=""
-        />
+ {/* IMAGE WAS HERE */}
       </div>
       <div className="min-w-0 flex-1">
         <form action="#">
